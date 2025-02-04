63e8db5b080afa3b53a7b0a145db0a62e88a0e50
MilosGak97 <127430859+MilosGak97@users.noreply.github.com>
2025-02-04 16:43:05 -0500

    m

--- src/api/properties/dto/filtering-response.dto.ts
+++ src/api/properties/dto/filtering-response.dto.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Property } from '../../../entities/property.entity';
+import { IsArray } from 'class-validator';
 
 export class FilteringResponseDto {
-  @ApiProperty({ isArray: true, type: [Property] })
+  @ApiProperty({ type: [Property] })
+  @IsArray()
   properties: Property[];
 
   @ApiProperty()
