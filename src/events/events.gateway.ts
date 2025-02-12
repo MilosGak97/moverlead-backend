9362b4a1c4514b2cc57afbdd65a95201c4c3fd54
MilosGak97 <127430859+MilosGak97@users.noreply.github.com>
2025-02-12 11:44:37 -0500

    m

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,4 +1,5 @@
 import {
+  ConnectedSocket,
   MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
@@ -7,25 +8,47 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
 
 @WebSocketGateway({ cors: Socket })
 export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  private logger = new Logger('EventsGateway');
+
   @WebSocketServer()
   server: Server;
 
   private clients = new Map<string, NodeJS.Timeout>();
 
+  @SubscribeMessage('ping')
+  handlePing(@ConnectedSocket() client: Socket) {
+    client.emit('ping');
+    this.logger.log(`Pong sent to ${client.id}`);
+
+    clearTimeout(this.clients.get(client.id));
+    this.clients.set(
+      client.id,
+      setTimeout(() => {
+        this.logger.warn(`Client ${client.id} unresponsive... Disconnected`);
+        client.disconnect();
+      }, 10000),
+    );
+  }
+
   handleConnection(client: Socket) {
-    console.log(`Client connected: ${client.id}`);
+    this.logger.log(`Client connected: ${client.id}`);
+
+    this.clients.set(
+      client.id,
+      setInterval(() => {
+        client.emit('ping');
+        this.logger.log(`Ping sent to ${client.id}`);
+      }, 5000),
+    );
   }
 
   handleDisconnect(client: Socket) {
-    console.log(`Client disconnected: ${client.id}`);
-  }
-
-  @SubscribeMessage('message')
-  handleMessage(@MessageBody() data: { text: string }) {
-    console.log('Received message:', data);
-    this.server.emit('message', { text: `Echo: ${data.text}` }); // Broadcast message
+    this.logger.log(`Client disconnected: ${client.id}`);
+    clearInterval(this.clients.get(client.id));
+    this.clients.delete(client.id);
   }
 }
